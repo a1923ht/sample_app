7c61ff78d7e89baa99d76270ad6f93fa24111b47
a1923ht <a1923ht@aiit.ac.jp>
2019-06-02 11:16:56 +0000

    Finish ch14

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,11 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   
   resources :users
   resources :account_activations, only: [:edit]
@@ -22,4 +27,5 @@ Rails.application.routes.draw do
   # Controller: paramas[:id]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
 end
